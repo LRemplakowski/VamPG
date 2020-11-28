0cd8c95c85c9758314f1c4fe0f9d5cd4d65ddf9c
Switchu <36130632+LRemplakowski@users.noreply.github.com>
2020-11-28 03:07:47 +0100

    basic animations

---
+++ Assets/Entities/Characters/Player/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : Vampire
+{
+    public override void Move(Vector3 moveTarget)
+    {
+        ClearAllActions();
+        AddActionToQueue(new Move(GetComponent<NavMeshAgent>(), moveTarget));
+    }
+
+    public void InteractWith(IInteractable target)
+    {
+        ClearAllActions();
+        AddActionToQueue(new Interact(target, this));
+    }
+}
---
+++ Assets/Entities/Scripts_Generic/CreatureAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreatureAnimator : MonoBehaviour
+{
+    const float movementAnimationSmoothTime = 0.1f;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float speedPercentage = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercentage", speedPercentage, movementAnimationSmoothTime, Time.deltaTime);
+    }
+}

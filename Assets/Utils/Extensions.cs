fd62d1bc0017a7fe5af191b1e7f85f7d8bcb5320
Switchu <36130632+LRemplakowski@users.noreply.github.com>
2022-03-24 13:28:32 +0100

    light tweaking

---
+++ Assets/Utils/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Extensions
+{
+    public static bool IsNullOrEmpty<T> (this T[] array)
+    {
+        if (array == null || array.Length == 0)
+            return true;
+        else
+            return Array.Exists(array, element => element != null);
+    }
+}
+

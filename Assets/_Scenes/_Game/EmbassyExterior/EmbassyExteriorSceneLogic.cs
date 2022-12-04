9a0a6622519e155c59fd7065e08fbcffde3ee44d
Switchu <36130632+LRemplakowski@users.noreply.github.com>
2022-12-04 01:57:12 +0100

    embassy navmesh setup

---
+++ Assets/_Scenes/_Game/EmbassyExterior/EmbassyExteriorSceneLogic.cs
@@ -0,0 +1,23 @@
+using SunsetSystems.Data;
+using SunsetSystems.Loading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Yarn.Unity;
+
+namespace SunsetSystems
+{
+    public class EmbassyExteriorSceneLogic : DefaultSceneLogic
+    {
+        [SerializeField]
+        private YarnProject _sceneDialogueProject;
+        [SerializeField]
+        private string _sceneStartDialogueNode;
+
+        public async override Task StartSceneAsync(SceneLoadingData data)
+        {
+            await base.StartSceneAsync(data);
+        }
+    }
+}

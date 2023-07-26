706c844551d5c1932556784a64a67f19501f558d
MorgothPalantir <morgothpalantir@gmail.com>
2023-07-26 16:43:51 +0200

    foliage shader

---
+++ Assets/_Content/_Shaders/WindzoneToShader.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+[ExecuteAlways]
+public class AlanWindZoneToShader : MonoBehaviour
+{
+    private WindZone windZone;
+
+    void Update()
+    {
+        ApplySettings();
+    }
+
+    void OnValidate()
+    {
+        ApplySettings();
+    }
+
+    void ApplySettings()
+    {
+        if (windZone == null)
+        {
+            windZone = gameObject.GetComponent<WindZone>();
+        }
+        if (windZone != null)
+        {
+            Shader.SetGlobalVector("_WINDZONE_Direction",
+                                   windZone.transform.forward);
+
+            // wind speed/strength (e.g. 1)
+            Shader.SetGlobalFloat("_WINDZONE_Main",
+                                  windZone.windMain);
+
+            // How many pulses of wind per second
+            Shader.SetGlobalFloat("_WINDZONE_Pulse_Frequency",
+                                  windZone.windPulseFrequency);
+
+            // Pulse strength, e.g. 0.5
+            Shader.SetGlobalFloat("_WINDZONE_Pulse_Magnitude",
+                                  windZone.windPulseMagnitude);
+
+            // Degree of variation (e.g. 1 is lots of variation)
+            Shader.SetGlobalFloat("_WINDZONE_Turbulence",
+                                  windZone.windTurbulence);
+        }
+    }
+}

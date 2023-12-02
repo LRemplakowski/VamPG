0271a716fb0e8f8d4cc1638b67b88bdd91d567b3
Switchu <36130632+LRemplakowski@users.noreply.github.com>
2023-12-02 14:27:46 +0100

    simple bullet spawning for weapons

--- Assets/_SunsetSystems/Inventory/WeaponInstance.cs
+++ Assets/_SunsetSystems/Inventory/WeaponInstance.cs
@@ -1,18 +1,27 @@
 using Sirenix.OdinInspector;
 using SunsetSystems.Animation;
 using SunsetSystems.Audio;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.VFX;
 
 namespace SunsetSystems.Equipment
 {
     public class WeaponInstance : SerializedMonoBehaviour, IWeaponInstance
     {
+        [Title("Config")]
+        [SerializeField, Min(0)]
+        private float bulletVelocity = 3f;
+        [SerializeField, Min(0)]
+        private float bulletLifetime = 5f;
         [Title("References")]
         [SerializeField]
         private Transform projectileOrigin;
         [SerializeField]
         private VisualEffect muzzleFlash;
+        [SerializeField]
+        private Rigidbody bulletPrefab;
         [SerializeField]
         private AudioClip shotSFX;
         [field: SerializeField]
@@ -25,6 +34,12 @@ namespace SunsetSystems.Equipment
         {
             if (muzzleFlash != null)
                 muzzleFlash.Play();
+            if (bulletPrefab)
+            {
+                Rigidbody bulletInstance = Instantiate(bulletPrefab, projectileOrigin.position, Quaternion.identity);
+                bulletInstance.AddForce(projectileOrigin.forward * bulletVelocity);
+                Destroy(bulletInstance.gameObject, bulletLifetime);
+            }
             if (shotSFX != null)
                 AudioManager.Instance.PlaySFXOneShot(shotSFX);
         }

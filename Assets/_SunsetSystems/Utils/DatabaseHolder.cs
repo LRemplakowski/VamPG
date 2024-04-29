760700d4791785b3eaedb8afa995c568e207ffd0
Switchu <remplak@gmail.com>
2024-04-29 18:34:49 +0200

    fuck you unity

--- Assets/_SunsetSystems/Utils/DatabaseHolder.cs
+++ Assets/_SunsetSystems/Utils/DatabaseHolder.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace SunsetSystems.Utils.Database
 {
-    [ExecuteAlways]
     public class DatabaseHolder : SerializedMonoBehaviour
     {
         public static DatabaseHolder Instance { get; private set; }

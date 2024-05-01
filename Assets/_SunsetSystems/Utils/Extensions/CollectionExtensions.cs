5ab85d2cf170f971017c1ea70adc2c5357d8ae51
Switchu <remplak@gmail.com>
2024-05-01 17:44:28 +0200

    Update CollectionExtensions.cs

--- Assets/_SunsetSystems/Utils/Extensions/CollectionExtensions.cs
+++ Assets/_SunsetSystems/Utils/Extensions/CollectionExtensions.cs
@@ -11,7 +11,11 @@ namespace SunsetSystems.Utils.Extensions
         {
             int enumerableCount = enumerable.Count();
             if (enumerableCount <= 0)
+            {
+                Debug.LogWarning($"Tried to get random element of type {nameof(T)}, but collection {enumerable} is empty!");
                 return default;
+            }
+
             int itemIndex = Random.Range(0, enumerableCount - 1);
             Debug.Log($"Got random item from collection {enumerable}! Item index: {itemIndex}");
             return enumerable.ElementAt(itemIndex);

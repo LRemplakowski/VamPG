282d578dc8e0ee48c6b07acae3244d3e268e8975
Switchu <remplak@gmail.com>
2024-12-27 20:26:18 +0100

    destroy all components on gameobject utility

---
+++ Assets/_SunsetSystems/Editor/ContextMenuUtilities.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace SunsetSystems.Utils.Editor 
+{ 
+    public static class ContextMenuUtilities
+    {
+        [MenuItem("CONTEXT/Component/Destroy All Components")] 
+        public static void DestroyAllComponentsExceptTransform(MenuCommand command) 
+        {
+            if (command.context is Component component)
+            {
+                var gameObject = component.gameObject;
+                var components = component.GetComponents<Component>();
+                foreach (var comp in components)
+                {
+                    if (comp is not Transform)
+                    {
+                        Object.DestroyImmediate(comp);
+                    }
+                }
+                EditorUtility.SetDirty(gameObject);
+            }
+        }
+    }
+}
